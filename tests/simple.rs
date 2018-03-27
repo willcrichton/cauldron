360303c0da405482df6ee834bbed00016cfce90f
Will Crichton <wcrichto@cs.stanford.edu>
2018-03-26 19:29:09 -0700

    Checkpoint

---
+++ tests/simple.rs
@@ -0,0 +1,49 @@
+#![feature(proc_macro)]
+
+extern crate cauldron;
+
+use cauldron::auto_gc;
+use std::rc::Rc;
+
+macro_rules! assert_eq_float {
+    ($e1:expr, $e2:expr, $eps:expr) => {
+        assert!(($e1 - $e2).abs() < $eps)
+    }
+}
+
+#[test]
+fn arith1() {
+    auto_gc! {
+        let x: i32 = 1;
+        let y = 1 + x;
+    };
+    assert_eq!(*y, 2);
+}
+
+#[test]
+fn arith2() {
+    auto_gc! {
+        let x = 1.0;
+        let y: f64 = 1.0 + x;
+    };
+    assert_eq_float!(2.0, *y, std::f64::EPSILON);
+}
+
+#[test]
+fn ifthen() {
+    auto_gc! {
+        let x = 1;
+        let y = if x == 1 { 1 } else { 2 };
+    };
+    assert_eq!(*y, 1);
+}
+
+#[test]
+fn closure() {
+    auto_gc! {
+        let x = 1;
+        let y = |z: i32| { x + z };
+        let z = y(x);
+    };
+    assert_eq!(*z, 2);
+}
